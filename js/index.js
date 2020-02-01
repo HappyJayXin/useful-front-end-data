7df43aef4e73bfa7b5cdb90b088edf82bea6bebe
Jay <4a490063@stust.edu.tw>
2020-02-01 23:04:25 +0800

    Feat: Add Search.

--- js/index.js
+++ js/index.js
@@ -2,12 +2,24 @@ new Vue({
   el: '#app',
   data() {
     return {
-      tabIndex: 0
+      tabIndex: 0,
+      sreachText: ''
     }
   },
   computed: {
     contents() {
       return [javascript, react, vue, css, image, other, article]
+    },
+    filteredSearch() {
+      return [
+        ...this.getFilter(javascript),
+        ...this.getFilter(react),
+        ...this.getFilter(vue),
+        ...this.getFilter(css),
+        ...this.getFilter(image),
+        ...this.getFilter(other),
+        ...this.getFilter(article)
+      ]
     }
   },
   watch: {
@@ -33,6 +45,25 @@ new Vue({
     init() {
       this.tabIndex = Number(localStorage.getItem('tabIndex')) || 0
     },
-    reverseArray: (...arr) => arr.forEach(arr => arr.reverse())
+    reverseArray: (...arr) => arr.forEach(arr => arr.reverse()),
+    getFilter({ lists }) {
+      return lists.filter(({ name }) => name.indexOf(this.sreachText) != -1);
+    },
+    handleSearch() {
+      if (this.sreachText === '' || this.filteredSearch.length < 1) {
+        this.makeToast('danger', '搜尋失敗。');
+        return;
+      }
+
+      const { href } = this.filteredSearch[0] || { href: null };
+      href && window.open(href, '_blank');
+    },
+    makeToast(variant = null, content = '') {
+      this.$bvToast.toast(content, {
+        title: '訊息',
+        variant: variant,
+        solid: true
+      })
+    }
   }
 })
