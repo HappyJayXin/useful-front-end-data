5762fcb512d8b3abfa60540b0dce3f840abbaedf
HappyJayXin <36494560+HappyJayXin@users.noreply.github.com>
2022-05-20 11:08:42 +0800

    Update typescript.js
    TypeScript Error Translator

--- data/typescript.js
+++ data/typescript.js
@@ -3,6 +3,10 @@ const typescript = {
   name: 'Typescript',
   lists: [
     {
+      name: 'TypeScript Error Translator',
+      href: 'https://ts-error-translator.vercel.app',
+    },
+    {
       name: 'type fest',
       href: 'https://github.com/sindresorhus/type-fest'
     }
